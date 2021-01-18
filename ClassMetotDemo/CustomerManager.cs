1a08c9dbefd6f8ebb5fcc21b8a0b6e52df58a027
Ömer Biltekin <omerbiltekin_@outlook.com>
2021-01-18 21:51:23 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void AddCustomer (Customer customer) 
+        {
+            Console.WriteLine("Customer added! : " + customer.Name + " " + customer.Surname);
+        }
+
+        public void DelCustomer(Customer customer) 
+        {
+            Console.WriteLine("Customer deleted ! : " + customer.Name + " " + customer.Surname);
+        }
+
+        public void ListCustomers(Customer[] customers) 
+        {
+            foreach (var item in customers)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.Surname);
+                Console.WriteLine(item.Id);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "Ömer";
+            customer1.Surname = "Biltekin";
+            customer1.Id = "1000000001";
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Caner";
+            customer2.Surname = "Yılmaz";
+            customer2.Id = "1000000002";
+
+            Customer customer3 = new Customer();
+            customer3.Name = "Ahmet";
+            customer3.Surname = "Su";
+            customer3.Id = "1000000003";
+
+            Customer customer4 = new Customer();
+            customer4.Name = "Selim";
+            customer4.Surname = "Meriç";
+            customer4.Id = "1000000004";
+
+            Customer[] customers = new Customer[] {customer1, customer2 , customer3 , customer4 };
+
+
+            CustomerManager customerManager = new CustomerManager();
+
+            Console.WriteLine("------ Customer Add ------");
+            customerManager.AddCustomer(customer1);
+            customerManager.AddCustomer(customer2);
+
+            Console.WriteLine("------ Customer Delete ------");
+            customerManager.DelCustomer(customer3);
+
+            Console.WriteLine("------ Customers List ------");
+            customerManager.ListCustomers(customers);
+
+
+        }
+    }
+}
